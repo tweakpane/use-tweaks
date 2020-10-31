ee65a4b6cc3368255b812b5ec4e381e841e22f27
David <david.bismut@gmail.com>
2020-10-31 13:48:56 +0100

    example: activate hmr

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
+if ('hot' in module) {
+  // @ts-ignore
+  module.hot.accept()
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
