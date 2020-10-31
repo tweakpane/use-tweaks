8e81725095db95ec9f5a6a14d2fef56bfe221601
David <david.bismut@gmail.com>
2020-10-31 20:36:14 +0100

    examples: upgrade to parcel 2 and Fast Refresh

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,9 +4,4 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-if ('hot' in module) {
-  // @ts-ignore
-  module.hot.accept()
-}
-
 ReactDOM.render(<App />, document.getElementById('root'))
