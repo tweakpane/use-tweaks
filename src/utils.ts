ce37a57954ff51785ef694db55c11aae3d105869
Yann Pringault <yann.pringault@gmail.com>
2020-11-06 10:59:30 +0100

    fix: useEffect when SSR

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import { useEffect, useLayoutEffect } from 'react'
+
 export function uuid(): string {
   return `${Math.floor((new Date().getTime() * Math.random()) / 1000)}`
 }
+
+export const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
